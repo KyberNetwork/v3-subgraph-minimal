056e9065f9da817f554ab86ebcba7e8974baaea3
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-09-12 18:13:11 +0700

    ft: etherex-cl

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x40059A6F242C3de0E639693973004921B04D96AD'
+export const FACTORY_ADDRESS = '0xAe334f70A7FC44FCC2df9e6A37BC032497Cf80f1'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
