5f94cf2e0ac90ae3ce2069d855c4c5af2919be07
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-11-15 02:46:52 +0700

    pinot-v3-monad

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { Factory as FactoryContract } from '../../generated/templates/Pool/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xAE6E5c62328ade73ceefD42228528b70c8157D0d'
+export const FACTORY_ADDRESS = '0xfd9c7369347E3b0a80e65223384831B2926ff4D0'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
